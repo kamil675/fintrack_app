eedfd4d3b74de60c14dcf2befca2709f74fef842
kamil675 <kamilmalik.mk.001@gmail.com>
2026-05-16 22:35:15 +0530

    cors fixed

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://your-frontend.vercel.app"],
+    origin: ["http://localhost:5173", "https://fintrack-app-sand.vercel.app"],
     credentials: true,
   }),
 );
